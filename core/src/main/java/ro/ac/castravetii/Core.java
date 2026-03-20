231000ec170f487bc2ab372f99e0512b4f56aebf
AndreiMit23 <andrei.m5@yahoo.com>
2026-03-20 09:45:35 +0200

    changed the Core //ai un milion pana marti?

--- core/src/main/java/ro/ac/castravetii/Core.java
+++ core/src/main/java/ro/ac/castravetii/Core.java
@@ -3,7 +3,6 @@ package ro.ac.castravetii;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core extends ApplicationAdapter {
@@ -13,19 +12,20 @@ public class Core extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
+
         image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+//        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+//        batch.draw(image, 140, 210);
         batch.end();
     }
 
     @Override
-    public void dispose() {
+        public void dispose() {
         batch.dispose();
         image.dispose();
     }
