75317fd3923638f6de722c618deaf04cd914ff33
Laurentiu-Laslau <laurentiu-leonard.laslau@student.tuiasi.ro>
2026-03-20 15:43:15 +0200

    Am adaugat comentarii in clasa Utils. Am pus un castravete rotitor pe ecran ca sa ne reprezinte echipa <3.

--- core/src/main/java/ro/ac/castravetii/Core.java
+++ core/src/main/java/ro/ac/castravetii/Core.java
@@ -1,37 +1,64 @@
 package ro.ac.castravetii;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Core extends ApplicationAdapter {
+import static com.badlogic.gdx.Gdx.gl;
 
-    private Sprite cubeSprite;
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Core implements ApplicationListener {
+
     private SpriteBatch spriteBatch;
     private TextureAtlas atlas;
-
+    private Sprite cubeSprite;
+    private Sprite castravete;
+    private Texture castraveteTexture;
 
     @Override
     public void create() {
         atlas = new TextureAtlas(Utils.getInternalPath("atlas/testAtlas.atlas"));
 
-        cubeSprite =  atlas.createSprite("stair");
         spriteBatch = new SpriteBatch();
+        cubeSprite =  atlas.createSprite("stair");
+        castraveteTexture = new Texture(Utils.getInternalPath("castravete.png"));
+        castravete = new Sprite(castraveteTexture);
+        castravete.setBounds(200, 120, 64*4, 64*4);
+        castravete.setOrigin(castravete.getHeight()/2, castravete.getWidth()/2);
+    }
+
+    @Override
+    public void resize(int width, int height) {
     }
 
     @Override
     public void render() {
+        castravete.setRotation(castravete.getRotation() + 1);
+
+        gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         spriteBatch.begin();
         cubeSprite.draw(spriteBatch);
+        castravete.draw(spriteBatch);
         spriteBatch.end();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
     }
 
     @Override
         public void dispose() {
         spriteBatch.dispose();
+        castraveteTexture.dispose();
         atlas.dispose();
     }
 
--- core/src/main/java/ro/ac/castravetii/Utils.java
+++ core/src/main/java/ro/ac/castravetii/Utils.java
@@ -3,19 +3,49 @@ package ro.ac.castravetii;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
+/**
+ * Clasa asta este facuta special ca sa nu trebuiasca sa scriem mereu Gdx.files.functie()
+ * Pentru mai multe informatii despre ce sunt chestiile de jos, uitati-va aici: <a href="https://libgdx.com/wiki/file-handling">File Handling Wiki</a>
+ */
 public class Utils {
+    /**
+     * Sincer nu stiu ce face functia asta, am vazut-o in tutorial si cica e folositoare
+     * Cine stie, poate o sa o folosim
+     *
+     * @param filepath Calea catre fisier
+     * @return un fisier cu extensia .class
+     */
     public static FileHandle getClassPath(String filepath) {
         return Gdx.files.classpath(filepath);
     }
 
+    /**
+     *  Folosim asta cand vrem sa utilizam un fisier din directorul proiectului. De exemplu, daca un fisier este in folderul /assets/nume_fisier.ext
+     *  il luam cu Utils.getInternalPath("nume_fisier.ext")
+     * @param filepath Calea catre fisier
+     * @return Un fisier din folder-ul proiectului
+     */
     public static FileHandle getInternalPath(String filepath) {
         return Gdx.files.internal(filepath);
     }
 
+    /**
+     * Folosim asta cand vrem sa utilizam un fisier din directorul home al calculatorului (de obicei C:\\users\\nume_utilizator\\)
+     *
+     * @param filepath Calea catre fisier
+     * @return  Fisier extern din C:\\users\nume_utilizator\\
+     */
     public static FileHandle getExternalPath(String filepath) {
         return Gdx.files.external(filepath);
     }
 
+    /**
+     * Aproape aceeasi chestie ca si getInternalPath() pentru desktop, pe mobile e altfel dar nu ne intereseaza.
+     * Probabil nu o sa folosim nici asta :)
+     *
+     * @param filepath Calea catre fisier
+     * @return Fisier local
+     */
     public static FileHandle getLocalPath(String filepath) {
         return Gdx.files.local(filepath);
     }
