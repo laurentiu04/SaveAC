870b4e2e5acdc622f964d62ea7119352d82156ee
Flavius <flavius.anton@student.tuiasi.ro>
2026-05-11 19:10:55 +0300

    Am adaugat ingame menu.

--- core/src/main/java/ro/ac/castravetii/screens/GameScreen.java
+++ core/src/main/java/ro/ac/castravetii/screens/GameScreen.java
@@ -2,12 +2,14 @@ package ro.ac.castravetii.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import ro.ac.castravetii.*;
 import ro.ac.castravetii.events.GameEventQueue;
 import ro.ac.castravetii.events.PlayerXPGainEvent;
 import ro.ac.castravetii.hud.HUD;
+import ro.ac.castravetii.hud.PauseMenu;
 import ro.ac.castravetii.systems.*;
 
 import static com.badlogic.gdx.Gdx.gl;
@@ -20,6 +22,10 @@ public class GameScreen implements Screen {
     public Player player;
     private MapGenerator mapGen;
     public float time = 0;
+
+    public enum GameState { RUNNING, PAUSED }
+    private GameState state = GameState.RUNNING;
+    private PauseMenu pauseMenu;
 
     public GameScreen(Game game, GameEventQueue queue) {
         this.game = game;
@@ -34,6 +40,8 @@ public class GameScreen implements Screen {
         player = Player.create(queue);
 
         hud = new HUD();
+
+        pauseMenu = new PauseMenu(this);
 
         Services.setCameraLimits(Services.MAP_WIDTH, Services.MAP_HEIGHT);
 
@@ -58,6 +66,14 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
 
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            if (state == GameState.RUNNING) {
+                pauseGame();
+            } else {
+                resumeGame();
+            }
+        }
+
         // Curat ecranul inainte sa desenez noul frame
         gl.glClearColor(0.696f, 0.733f, 0.780f, 1f);
         gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -66,24 +82,42 @@ public class GameScreen implements Screen {
         mapGen.render();
         Services.batch.end();
 
-        time += delta;
-        if (time >= 2f) {
-            time = 0;
-            queue.post(new PlayerXPGainEvent(10));
+        if (state == GameState.RUNNING) {
+            time += delta;
+            if (time >= 2f) {
+                time = 0;
+                queue.post(new PlayerXPGainEvent(10));
+            }
         }
 
-        Services.engine.update(Gdx.graphics.getDeltaTime());
+        float engineDelta = (state == GameState.RUNNING) ? delta : 0f;
+        Services.engine.update(engineDelta);
 
         player.snapCamera();
-
         Services.batch.setProjectionMatrix(Services.camera.combined);
-
         Services.shapeRenderer.setProjectionMatrix(Services.camera.combined);
 
         hud.render();
 
+        if (state == GameState.PAUSED) {
+            pauseMenu.getStage().act(delta);
+            pauseMenu.getStage().draw();
+        }
+
         queue.clearAll();
+
     }
+
+    public void pauseGame() {
+        state = GameState.PAUSED;
+        Gdx.input.setInputProcessor(pauseMenu.getStage());
+    }
+
+    public void resumeGame() {
+        state = GameState.RUNNING;
+        Gdx.input.setInputProcessor(null);
+    }
+
 
     @Override
     public void resize(int width, int height) {
@@ -96,12 +130,15 @@ public class GameScreen implements Screen {
 
         hud.stage.getViewport().update(width, height, true);
         Services.setCameraLimits(Services.MAP_WIDTH, Services.MAP_HEIGHT);
+        pauseMenu.getStage().getViewport().update(width, height, true);
 
     }
 
     @Override
     public void pause() {
-
+        if (state == GameState.RUNNING) {
+            pauseGame();
+        }
     }
 
     @Override
@@ -119,5 +156,6 @@ public class GameScreen implements Screen {
         mapGen.dispose();
         Services.dispose();
         hud.dispose();
+        pauseMenu.dispose();
     }
 }
