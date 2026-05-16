581c921f998057ff5a9f35505112c2212abbfd09
AndreiMit23 <andrei.m5@yahoo.com>
2026-05-16 17:27:04 +0300

    Added new EnemyBoss class & implemented Wave message, fixed enemy stacking
    (cherry picked from commit 97fc85aad83292c2e056b4f98577ff0645747123)

--- core/src/main/java/ro/ac/castravetii/systems/EnemyWaveSystem.java
+++ core/src/main/java/ro/ac/castravetii/systems/EnemyWaveSystem.java
@@ -1,34 +1,93 @@
 package ro.ac.castravetii.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
-import ro.ac.castravetii.BellPepperEnemy;
-import ro.ac.castravetii.Enemy;
-import ro.ac.castravetii.PepperEnemy;
+import com.badlogic.ashley.core.Family;
+import ro.ac.castravetii.*;
+import ro.ac.castravetii.components.EnemyComponent;
 import ro.ac.castravetii.components.TransformComponent;
+import ro.ac.castravetii.hud.HUD;
+
 
 public class EnemyWaveSystem extends EntitySystem {
     private float counter = 0f;
 
     //how many Enemies are spawned in current wave.
     private int spawnedInWave = 0;
-    private int wave = 1;
-    private int maxWaves = 5;
+    private int wave = 0;
+    private final int maxWaves = 5;
+    public boolean spawningFinished = false;
+    private int maxPerWave = 15;
+
+    private final HUD hud;
+    public float TimerMessage = 0f;
+
+    public EnemyWaveSystem(HUD hud) {
+        this.hud = hud;
+
+        wave++;
+
+        hud.getWaveLabel().setText("WAVE 1");
+        hud.getWaveLabel().setVisible(true);
+
+        TimerMessage = 3f;
+    }
+
+    public boolean isEnemyAlive(){
+        return getEngine().getEntitiesFor(Family.all(EnemyComponent.class).get()).size()>0;
+    }
 
     @Override
     public void update(float deltaTime){
         counter += deltaTime;
 
-        int maxPerWave = 2;
-
         //every 3 seconds enemies appear.
         float spawnInterval = 0.3f;
+
+        if(spawningFinished && !isEnemyAlive()){
+            wave++;
+
+            hud.getWaveLabel().setText("WAVE " + wave);
+
+            hud.getWaveLabel().setVisible(true);
+
+            TimerMessage = 3f;
+
+            spawnedInWave = 0;
+
+            maxPerWave += 5;
+
+            spawningFinished = false;
+        }
+
+        if(TimerMessage > 0){
+            TimerMessage -= deltaTime;
+
+            if(TimerMessage <= 0){
+                hud.getWaveLabel().setVisible(false);
+            }
+        }
 
         if(counter >= spawnInterval && spawnedInWave < maxPerWave){
             counter = 0f;
             //the map is 50x32, but I want a spawn point smaller because it is going to be boring
             //to wait for the enemies to come at player for a long period of time if they are spawned in other corner of the map.
-            float x = (float) (Math.random() * 200) + 200f;
-            float y = (float) (Math.random() * 800);
+            float distance;
+            float x;
+            float y;
+
+            // spawn pentru inamici de la o distanta considerabila <3
+            do {
+                x = (float) (Math.random() * 800);
+                y = (float) (Math.random() * 800);
+
+                TransformComponent player = Player.getInstance().getTransformComponent();
+
+                float dx = x - player.position.x;
+                float dy = y - player.position.y;
+
+                distance = (float) Math.sqrt(dx * dx + dy * dy);
+
+            }while(distance < 300f);
 
             Enemy enemy;
 
@@ -36,6 +95,12 @@ public class EnemyWaveSystem extends EntitySystem {
                 enemy = new PepperEnemy();
             }else if(wave == 2){
                 enemy = new BellPepperEnemy();
+            }else if(wave == 3){
+                enemy = new TomatoEnemy();
+            }else if(wave == 4){
+                enemy = new EnemyBoss();
+
+                maxPerWave = 1;
             }else{
                 return;
             }
@@ -45,11 +110,8 @@ public class EnemyWaveSystem extends EntitySystem {
             tc.position.y = y;
 
             spawnedInWave++;
-
             if(spawnedInWave >= maxPerWave){
-                wave++;
-                spawnedInWave = 0;
-                maxPerWave += 5;
+                spawningFinished = true;
             }
         }
     }
